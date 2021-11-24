5ba0a55037687ecd4f058bd68e5ff5ef9868c0bf
ojayjonathan <jonathanonderi2018@yahoo.com>
2021-11-24 13:40:42 +0300

    login intergration

--- src/utils/services/auth.services.js
+++ src/utils/services/auth.services.js
@@ -1,10 +1,13 @@
-export const login = async (email, password) => {
+import { IP_ADDRESS } from "../constants";
+import { storeToLocalstorage } from "../utils";
+
+export const login = async (credential) => {
   const result = await fetch(IP_ADDRESS + "admin/user/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify(credential),
   }).catch((e) => {
     return e;
   });
@@ -61,65 +64,65 @@ export const FetchUsers = async () => {
   }
 };
 
-export const registerNewUser = async (user) => {
-  const profile = await createDefaultProfilePic(user.firstName, user.lastName);
-  const formData = new FormData();
-  for (const field in user) {
-    if (Object.hasOwnProperty.call(user, field)) {
-      const value = user[field];
-      if (field !== "profilePic") {
-        formData.append(field, value);
-      }
-    }
-  }
-  formData.append("profilePic", profile, profile.name);
+// export const registerNewUser = async (user) => {
+//   const profile = await createDefaultProfilePic(user.firstName, user.lastName);
+//   const formData = new FormData();
+//   for (const field in user) {
+//     if (Object.hasOwnProperty.call(user, field)) {
+//       const value = user[field];
+//       if (field !== "profilePic") {
+//         formData.append(field, value);
+//       }
+//     }
+//   }
+//   formData.append("profilePic", profile, profile.name);
 
-  let request = new XMLHttpRequest();
-  let result;
-  request.open("POST", IP_ADDRESS + "/auth/register");
-  request.send(formData);
-  return new Promise((resolve, reject) => {
-    request.onload = () => {
-      result = JSON.parse(request.response);
-      resolve(result);
-    };
-    request.onerror = () => {
-      resolve({
-        message: "An error occured please check your internet connection",
-        status: request.status,
-      });
-    };
-    request.ontimeout = () => {
-      resolve({
-        message: "An error occured please check your internet connection",
-        status: request.status,
-      });
-    };
-  });
-};
+//   let request = new XMLHttpRequest();
+//   let result;
+//   request.open("POST", IP_ADDRESS + "/auth/register");
+//   request.send(formData);
+//   return new Promise((resolve, reject) => {
+//     request.onload = () => {
+//       result = JSON.parse(request.response);
+//       resolve(result);
+//     };
+//     request.onerror = () => {
+//       resolve({
+//         message: "An error occured please check your internet connection",
+//         status: request.status,
+//       });
+//     };
+//     request.ontimeout = () => {
+//       resolve({
+//         message: "An error occured please check your internet connection",
+//         status: request.status,
+//       });
+//     };
+//   });
+// };
 
-/**
- *
- * @param {Object.<string,any>} updates - user  profile fields to update
- * @returns
- */
+// /**
+//  *
+//  * @param {Object.<string,any>} updates - user  profile fields to update
+//  * @returns
+//  */
 
-export const updateProfile = async (updates) => {
-  const result = await fetch(IP_ADDRESS + "/auth/profile", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(updates),
-  }).catch((e) => {
-    return e;
-  });
-  const data = await result.json();
-  if (result.ok) {
-    return data;
-  } else {
-    return {
-      message: data.message || "Error updating profile please retry",
-    };
-  }
-};
+// export const updateProfile = async (updates) => {
+//   const result = await fetch(IP_ADDRESS + "/auth/profile", {
+//     method: "PUT",
+//     headers: {
+//       "Content-Type": "application/json;charset=utf-8",
+//     },
+//     body: JSON.stringify(updates),
+//   }).catch((e) => {
+//     return e;
+//   });
+//   const data = await result.json();
+//   if (result.ok) {
+//     return data;
+//   } else {
+//     return {
+//       message: data.message || "Error updating profile please retry",
+//     };
+//   }
+// };
